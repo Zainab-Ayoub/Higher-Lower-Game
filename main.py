2eaaad738a0fc242e6b076d0a5b55d307f6fd421
Zainab-Ayoub <zainabzafar365@gmail.com>
2025-02-09 18:46:23 +0500

    final commit

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from art import logo, vs
 from gameData import data
+import os 
 import random
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 print(logo)
 score = 0
 gameShouldContinue = True
+accountB = random.choice(data)
 
 def formatData(account):
     accountName = account["name"]
     accountDescription = account["description"]
     accountCountry = account["country"]
-    print(f"{accountName}, a {accountDescription}, from {accountCountry}")
+    return f"{accountName}, a {accountDescription}, from {accountCountry}"
 
 def checkAnswer(guess, a_follower_count, b_follower_count):
     if a_follower_count > b_follower_count:
@@ -18,23 +23,27 @@ def checkAnswer(guess, a_follower_count, b_follower_count):
     else:
         return guess == "b"
 while gameShouldContinue:
-    accountA = random.choice(data)
+    accountA = accountB
     accountB = random.choice(data)
-    if accountA == accountB:
+    
+    while accountA == accountB:
         accountB = random.choice(data)    
 
     print(f"Compare A: {formatData(accountA)}.")    
     print(vs)
-    print(f"Again B: {formatData(accountB)}.")   
+    print(f"Against B: {formatData(accountB)}.")   
 
     guess = input("Who has more followers? Type 'A' or 'B': ").lower()
 
     a_follower_count = accountA["follower_count"]
     b_follower_count = accountB["follower_count"]
     isCorrect = checkAnswer(guess, a_follower_count, b_follower_count)
+    clear()
+    print(logo)
 
     if isCorrect:
         score += 1
-        print(f"You're right! Current core: {score}.")
+        print(f"You're right! Current score: {score}.")
     else:
-        print(f"Sorry, that's wrong. Final score: {score}.")    
+        print(f"Sorry, that's wrong. Final score: {score}.")  
+        gameShouldContinue = False  
